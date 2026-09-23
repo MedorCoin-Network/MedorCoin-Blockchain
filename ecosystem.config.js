7a10677c7de5bc71100a033678e9823d208a0301
Ajvlogger <ajvlogger4@gmail.com>
2026-09-23 17:17:58 +0800

    Update ecosystem.config.js
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,24 @@
 module.exports = {
   apps: [
-    { name: "redis-6379", script: "redis-server", args: "--port 6379 --cluster-enabled yes --daemonize no" },
-    { name: "redis-6380", script: "redis-server", args: "--port 6380 --cluster-enabled yes --daemonize no" },
-    { name: "redis-6381", script: "redis-server", args: "--port 6381 --cluster-enabled yes --daemonize no" },
-    { name: "medor-engine", script: "npm start", delay: 5000 }
+    { 
+      name: "redis-6379", 
+      script: "redis-server", 
+      args: "--port 6379 --cluster-enabled yes --cluster-config-file nodes-6379.conf --daemonize no" 
+    },
+    { 
+      name: "redis-6380", 
+      script: "redis-server", 
+      args: "--port 6380 --cluster-enabled yes --cluster-config-file nodes-6380.conf --daemonize no" 
+    },
+    { 
+      name: "redis-6381", 
+      script: "redis-server", 
+      args: "--port 6381 --cluster-enabled yes --cluster-config-file nodes-6381.conf --daemonize no" 
+    },
+    { 
+      name: "medor-engine", 
+      script: "npm start", 
+      delay: 5000 
+    }
   ]
 };
