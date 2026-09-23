f91e3c78b36be9691e573c85b7502f409aadc4a6
Ajvlogger <ajvlogger4@gmail.com>
2026-09-23 21:23:35 +0800

    Create token_config.js
    Signed-off-by: Ajvlogger <ajvlogger4@gmail.com>

---
+++ public-frontend/token_config.js
@@ -0,0 +1,11 @@
+// MedorCoin Network Asset Registration Parameters
+const tokenDetails = {
+    address: "0x0cadd7ec2943eabaf6a4e2e8b20e77fd31179895",
+    symbol: "MEDA",
+    decimals: 18,
+    name: "Medac",
+    networkFloorPrice: "\$0.00014286",
+    fixedExchangeRate: "3,000 MEDA = 1 BTC"
+};
+
+console.log(`[Ecosystem Link] Frontend dashboard connected to verified asset: ${tokenDetails.name} (${tokenDetails.symbol})`);
